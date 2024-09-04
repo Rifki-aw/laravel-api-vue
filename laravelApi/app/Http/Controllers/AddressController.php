b1a0255f85022f2277c044dfee9025ab9af7e1a3
rifki <rifkibahrululum@gmail.com>
2024-09-04 16:04:47 +0700

    Refactor Code for Get Contact and Get Address

--- laravelApi/app/Http/Controllers/AddressController.php
+++ laravelApi/app/Http/Controllers/AddressController.php
@@ -2,21 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Address;
 use App\Models\Contact;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\AddressCreateRequest;
 use App\Http\Resources\AddressResource;
+use App\Http\Requests\AddressCreateRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AddressController extends Controller
 {
-    public function create(int $idContact, AddressCreateRequest $request): JsonResponse
+    private function getContact(User $user, int $idContact): Contact 
     {
-        // Cek dan ambil user yang sedang login
-        $user = Auth::user();
-
         // Ambil data contact yang sesuai dengan user yang sedang login dan ID contact yang diberikan
         $contact = Contact::where('user_id', $user->id)->where('id', $idContact)->first();
         
@@ -31,6 +29,37 @@ class AddressController extends Controller
                 ]
             ])->setStatusCode(404));
         }
+
+        return $contact;
+    }
+
+    private function getAddress(Contact $contact, int $idAddress): Address
+    {
+        // Ambil data address yang sesuai dengan contact
+        $address = Address::where('contact_id', $contact->id)->where('id', $idAddress)->first();
+
+        // Cek jika data contact tidak ditemukan di database
+        if (!$address) {
+            // Jika contact tidak ditemukan, lemparkan error 404 dengan pesan "not found"
+            throw new HttpResponseException(response()->json([
+                "errors" => [
+                    "message" => [
+                        "not found"
+                    ]
+                ]
+            ])->setStatusCode(404));
+        }
+
+        return $address;
+    }
+
+    public function create(int $idContact, AddressCreateRequest $request): JsonResponse
+    {
+        // Cek dan ambil user yang sedang login
+        $user = Auth::user();
+
+        // Ambil data contact yang sesuai dengan user yang sedang login dan ID contact yang diberikan
+        $contact = $this->getContact($user, $idContact);
 
         // Ambil data yang sudah tervalidasi dari request
         $data = $request->validated();
@@ -48,37 +77,13 @@ class AddressController extends Controller
     public function get(int $idContact, int $idAddress): AddressResource
     {
         // Cek dan ambil user yang sedang login
-        $user = Auth::getUser();
+        $user = Auth::user();
 
         // Ambil data contact yang sesuai dengan user yang sedang login dan ID contact yang diberikan
-        $contact = Contact::where('user_id', $user->id)->where('id', $idContact)->first();
-
-        // Cek jika data contact tidak ditemukan di database
-        if (!$contact) {
-            // Jika contact tidak ditemukan, lemparkan error 404 dengan pesan "not found"
-            throw new HttpResponseException(response()->json([
-                "errors" => [
-                    "message" => [
-                        "not found"
-                    ]
-                ]
-            ])->setStatusCode(404));
-        }
+        $contact = $contact = $this->getContact($user, $idContact);
 
         // Ambil data address yang sesuai dengan contact
-        $address = Address::where('contact_id', $contact->id)->where('id', $idAddress)->first();
-
-        // Cek jika data contact tidak ditemukan di database
-        if (!$address) {
-            // Jika contact tidak ditemukan, lemparkan error 404 dengan pesan "not found"
-            throw new HttpResponseException(response()->json([
-                "errors" => [
-                    "message" => [
-                        "not found"
-                    ]
-                ]
-            ])->setStatusCode(404));
-        }
+        $address = $this->getAddress($contact, $idAddress);
 
         return new AddressResource($address);
     }
