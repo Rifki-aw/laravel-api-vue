3fa43e38a9fe98bf5f154504e00e59b89c099418
rifki <rifkibahrululum@gmail.com>
2024-09-19 18:18:11 +0700

    Login Page

---
+++ laravelApi/frontend/src/views/auth/login.vue
@@ -0,0 +1,130 @@
+<script setup></script>
+
+<template>
+    <div class="login">
+        <div class="container" style="margin-top: 100px">
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <div v-if="loginFailed" class="alert-danger alert">
+                        Username atau Password Anda Salah
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            LOGIN
+                            <hr />
+                            <form @submit.prevent="login">
+                                <div class="form-group">
+                                    <label for="username">USERNAME</label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="Masukkan Username"
+                                        v-model="user.username"
+                                    />
+                                    <div
+                                        class="mt-2 alert alert-danger"
+                                        v-if="validation.username"
+                                    >
+                                        Masukkan Username
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password">PASSWORD</label>
+                                    <input
+                                        type="password"
+                                        class="form-control"
+                                        placeholder="Masukkan Password"
+                                        v-model="user.password"
+                                    />
+                                    <div
+                                        v-if="validation.password"
+                                        class="mt-2 alert alert-danger"
+                                    >
+                                        Masukkan Password
+                                    </div>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">
+                                    LOGIN
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import api from "../../api";
+
+export default {
+    name: "login",
+    data() {
+        return {
+            loggedIn: localStorage.getItem("loggedIn"),
+            token: localStorage.getItem("token"),
+            user: [],
+            validation: [],
+            loginFailed: null,
+        };
+    },
+    methods: {
+        login() {
+            api.post("/api/users/login", {
+                username: this.user.username,
+                password: this.user.password,
+            })
+                .then((response) => {
+                    //debug cookie
+                    console.log(response);
+
+                    if (response.data.data.token) {
+                        // set localStorage untuk menandai user telah login
+                        localStorage.setItem("loggedIn", "true"),
+                            // set localStorage token pada user yang login
+                            localStorage.setItem(
+                                "token",
+                                response.data.data.token
+                            );
+
+                        // change state
+                        // this.loggedIn = true;
+                        this.$emit('login');
+
+                        // redirect ke dashboard
+                        return this.$router.push({ name: "dashboard" });
+                    } 
+                    // else {
+                    //     // set state login failed
+                    //     this.loginFailed = true;
+                    // }
+                })
+                .catch((error) => {
+                    if (error.response.status === 401) {
+                        this.loginFailed = true;
+                    } else {
+                        console.log(error);
+                    }
+                });
+
+            this.validation = [];
+
+            if (!this.user.username) {
+                this.validation.username = true;
+            }
+            if (!this.user.password) {
+                this.validation.password = true;
+            }
+        },
+
+        mounted() {
+            if (this.loggedIn) {
+                return this.$router.push({ name: "dashboard" });
+            }
+        },
+    },
+};
+</script>
