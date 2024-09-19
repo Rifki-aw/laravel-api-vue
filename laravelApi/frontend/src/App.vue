cc295b09653e6c0efdacbd696566b476f64d6f0a
rifki <rifkibahrululum@gmail.com>
2024-09-19 18:14:10 +0700

    Navbar
    Membuat navbar yang dinamis

--- laravelApi/frontend/src/App.vue
+++ laravelApi/frontend/src/App.vue
@@ -1,30 +1,91 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div>
+        <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+            <div class="container">
+                <router-link :to="{ name: 'dashboard' }" class="navbar-brand"
+                    >DASHBOARD</router-link
+                >
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    class="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item" v-if="!loggedIn">
+                            <router-link
+                                :to="{ name: 'login' }"
+                                class="nav-link active"
+                                aria-current="page"
+                                >Login</router-link
+                            >
+                        </li>
+                        <li class="nav-item" v-if="loggedIn">
+                            <span class="nav-link active" aria-current="page">{{
+                                user.name
+                            }}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!--- render router view -->
+        <!-- <router-view></router-view> -->
+        <router-view @login="handleLogin" @logout="handleLogout"></router-view>
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import api from "./api";
+
+export default {
+    name: "App",
+    data() {
+        return {
+            loggedIn: localStorage.getItem("loggedIn"),
+            token: localStorage.getItem("token"),
+            user: [],
+        };
+    },
+    mounted() {
+        if (this.loggedIn) {
+            this.getUserData();
+            this.$router.push({ name: "dashboard" });
+        }
+    },
+    methods: {
+        getUserData() {
+            api.get("/api/users/current", {
+                headers: { Authorization: this.token },
+            })
+                .then((response) => {
+                    // console.log(response);
+                    this.user = response.data.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        handleLogin() {
+            this.loggedIn = true;
+            this.token = localStorage.getItem("token");
+            this.getUserData();
+        },
+        handleLogout() {
+            this.loggedIn = false;
+            this.token = null;
+            this.user = [];
+        },
+    },
+};
+</script>
