b8ddb57f4325858827b5594f007dfac354fdc31a
rifki <rifkibahrululum@gmail.com>
2024-09-19 18:18:22 +0700

    Dashboard Page

---
+++ laravelApi/frontend/src/views/home/index.vue
@@ -0,0 +1,134 @@
+<!-- Versi 3.X -->
+<script setup>
+import { ref, onMounted } from "vue";
+import api from "../../api";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const token = ref(localStorage.getItem("token"));
+const user = ref({});
+
+const emit = defineEmits(['logout']);
+
+onMounted(async () => {
+    try {
+        const response = await api.get("/api/users/current", {
+            headers: { Authorization: token.value },
+        });
+        user.value = response.data.data;
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+function logout() {
+    api.delete("/api/users/logout", {
+        headers: { Authorization: token.value },
+    })
+        .then((response) => {
+            console.log("Logout success:", response.data.data);
+
+            // remove localStorage item
+            localStorage.removeItem("token");
+            localStorage.removeItem("loggedIn");
+
+            emit('logout');
+
+            // redirect to login page
+            router.push({ name: "login" });
+        })
+        .catch((error) => {
+            console.error("Logout failed:", error);
+        });
+}
+</script>
+
+<template>
+    <div class="dashboard" style="margin-top: 80px">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            MAIN MENU
+                            <hr />
+                            <ul class="list-group">
+                                <router-link
+                                    :to="{ name: 'dashboard' }"
+                                    class="list-group-item text-dark text-decoration-none"
+                                    >DASHBOARD</router-link
+                                >
+                                <li
+                                    @click="logout"
+                                    class="list-group-item text-dark text-decoration-none"
+                                    style="cursor: pointer"
+                                >
+                                    LOGOUT
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+                            DASHBOARD
+                            <hr />
+                            Selamat Datang <strong>{{ user.name }}</strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<!-- Versi 2.X  -->
+<!-- <script>
+import api from "../../api";
+
+export default {
+    name: "Dashboard",
+    data() {
+        return {
+            loggedIn: localStorage.getItem("loggedIn"),
+            token: localStorage.getItem("token"),
+            user: [],
+        };
+    },
+    created() {
+        api.get("/api/users/current", {
+            headers: { Authorization: this.token },
+        })
+            .then((response) => {
+                // console.log(response);
+                this.user = response.data.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    methods: {
+        logout() {
+            api.delete("/api/users/logout", {
+                headers: { Authorization: this.token },
+            })
+                .then((response) => {
+                    console.log("Logout success:", response.data.data);
+
+                    // remove localStorage item
+                    localStorage.removeItem("token");
+                    localStorage.removeItem("loggedIn");
+
+                    // redirect to login page
+                    this.$router.push({ name: "login" });
+                })
+                .catch((error) => {
+                    console.error("Logout failed:", error);
+                });
+        },
+    },
+    // mounted() {},
+};
+</script> -->
